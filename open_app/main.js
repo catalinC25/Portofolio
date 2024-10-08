cc2b587c07a8c97a7b85774ba5a618b589e903ec
catalinC25 <andreipolic8@gmail.com>
2024-10-08 20:45:02 +0300

    Implementare functionalitati: 1. Adaugare like 2. Eliminare postare cand butonul X aferent postarii este apasat 3. Functionalitate adaugare comentariu + incrementarea numarului de comentarii

--- open_app/main.js
+++ open_app/main.js
@@ -1,30 +1,77 @@
 
-// Conversion ===================================
-function textToNumber(element) {
-    let myValue = parseInt(element.textContent);
-    return myValue;
+// Add likes ===================================
+
+// actual numbers of likes
+let likeCount = totalLikes.textContent;
+
+function likesManager() {
+    if (totalLikesText.textContent === "Like") {
+        // add like to count
+        likeCount ++;
+        totalLikes.textContent = likeCount;
+        // change "Like" text
+        totalLikesText.textContent = "Liked";
+        // change text color
+        totalLikesText.style.color = "#176A73";
+    } else {
+        // add like to count
+        likeCount --;
+        totalLikes.textContent = likeCount;
+        // change "Like" text
+        totalLikesText.textContent = "Like";
+        totalLikesText.style.color = "#0D0D0D";
+    }
 }
 
-function numberToText(element) {
-    let myElement = element;
-    let myText = myElement.toString();
-    return(myText);
+// Remove post ===================================
+// when click on "X" from inside post icon, remove the post.
+function postClose () {
+    mainPost.style.display="none";
 }
 
-// Main functionality
 
-function addLike() {
-    let currentLikes = textToNumber(totalLikes);
-    let colorText = "#176A73";
-    currentLikes++;
-    totalLikes.textContent = currentLikes;
-    // likeBtnText.style.color = colorText;
-    // if (colorText === "#176A73") {
-    //     colorText = "#202E40";
-    // } else {
-    //     colorText = "#176A73";
-    // }
+// Add comments ===================================
+// actual number of comments
+let totalComments = numberOfComments.textContent;
 
+function addComment () {
+
+    // get the comment input
+    const enteredComment = commentInput.value;
+
+    // CREATE first div for username and entered comment
+    const userAndComment = document.createElement("div");
+    userAndComment.classList.add("commentText");
+    // create user name
+    const newUsername = document.createElement('h4');
+    newUsername.classList.add("userComment");
+    newUsername.textContent = mainUserName.textContent;
+    // create new comment
+    const newCommentText = document.createElement("h3");
+    newCommentText.classList.add("userContent");
+    newCommentText.textContent = enteredComment;
+    // append elements to the new comment
+    userAndComment.appendChild(newUsername);
+    userAndComment.appendChild(newCommentText);
+
+    // CREATE second div to hold the first pack from line 34 and the new avatar img element
+    const fullComment = document.createElement("div");
+    fullComment.classList.add("firstComment");
+    // create new avatar img
+    const newAvatar = document.createElement("img");
+    newAvatar.src="/Graphs/main_avatar.jpg";
+    newAvatar.classList.add('mainAvatar');
+    // line 56 appent above avatar img, line 57 append the pack created in line 34
+    fullComment.appendChild(newAvatar);
+    fullComment.appendChild(userAndComment);
+    // add the full content in html post
+    commentsSection.appendChild(fullComment);
+
+    // Increment the number of comments when a new comment is entered
+    totalComments++;
+    numberOfComments.textContent = totalComments;
 }
 
-likeButton.addEventListener('click', addLike);
+likeButton.addEventListener('click', likesManager);
+closePost.addEventListener("click", postClose);
+commentSubmit.addEventListener("click", addComment);
--- open_app/vendor.js
+++ open_app/vendor.js
@@ -1,5 +1,14 @@
-let totalLikes = document.getElementById("totalLikes");
+// User
+const mainUserName = document.getElementById("mainUser");
+// Likes
+const totalLikes = document.getElementById("totalLikes");
 const totalLikesText = document.getElementById("likeBtnText");
-
 const likeButton = document.getElementById("actionLike");
-
+// Flow posts
+const closePost = document.getElementById("closePostBtn");
+const mainPost = document.getElementById("mainPost");
+// Comments from posts
+const postCommentInput = document.getElementById("commentInput");
+const commentSubmit = document.getElementById("commentSubmit");
+const commentsSection = document.getElementById("commentsSection");
+const numberOfComments = document.getElementById("numberOfComments");
