b6b7b95118391a17f1be0c02c2e9c1598eeb976b
catalinC25 <andreipolic8@gmail.com>
2024-10-14 17:49:20 +0300

    carusel story, in lucru

--- open_app/main.js
+++ open_app/main.js
@@ -1,77 +1,118 @@
 
-// Add likes ===================================
+// Adaugare like ===================================
 
-// actual numbers of likes
+// Numarul actua de like urii
 let likeCount = totalLikes.textContent;
 
 function likesManager() {
     if (totalLikesText.textContent === "Like") {
-        // add like to count
+        // adaugare like in totalul numarului de like uri
         likeCount ++;
         totalLikes.textContent = likeCount;
-        // change "Like" text
+        // schimbare text "Like" in "Liked"
         totalLikesText.textContent = "Liked";
-        // change text color
+        // schimbare culoare text
         totalLikesText.style.color = "#176A73";
     } else {
-        // add like to count
+        // eliminare like din numaratoare
         likeCount --;
         totalLikes.textContent = likeCount;
-        // change "Like" text
+        // schimbare text in "Like" si culoare textului in negru
         totalLikesText.textContent = "Like";
         totalLikesText.style.color = "#0D0D0D";
     }
 }
 
-// Remove post ===================================
-// when click on "X" from inside post icon, remove the post.
+// Eliminare postare ===================================
+// la apasarea butonului "X" aferent postarii, aceasta este eliminata
 function postClose () {
     mainPost.style.display="none";
 }
 
 
-// Add comments ===================================
-// actual number of comments
+// Adaugare comentarii ===================================
+// extragerea numarului actual de comentarii
 let totalComments = numberOfComments.textContent;
 
 function addComment () {
 
-    // get the comment input
+    // identificare input pentru comentarii
     const enteredComment = commentInput.value;
 
-    // CREATE first div for username and entered comment
-    const userAndComment = document.createElement("div");
-    userAndComment.classList.add("commentText");
-    // create user name
-    const newUsername = document.createElement('h4');
-    newUsername.classList.add("userComment");
-    newUsername.textContent = mainUserName.textContent;
-    // create new comment
-    const newCommentText = document.createElement("h3");
-    newCommentText.classList.add("userContent");
-    newCommentText.textContent = enteredComment;
-    // append elements to the new comment
-    userAndComment.appendChild(newUsername);
-    userAndComment.appendChild(newCommentText);
+    // conditie creata, daca nu este text in inputul de comentarii, functia nu se executa
+    if (enteredComment) {
+            
+        // crearea primului div pentru username si comentariu inputat
+        const userAndComment = document.createElement("div");
+        userAndComment.classList.add("commentText");
+        // creaza username
+        const newUsername = document.createElement('h4');
+        newUsername.classList.add("userComment");
+        newUsername.textContent = mainUserName.textContent;
+        // creaza comentariu ou
+        const newCommentText = document.createElement("h3");
+        newCommentText.classList.add("userContent");
+        newCommentText.textContent = enteredComment;
+        // adaugare elemente la comntariul nou (elementele de mai sus adaugate in constanta din linia 45)
+        userAndComment.appendChild(newUsername);
+        userAndComment.appendChild(newCommentText);
 
-    // CREATE second div to hold the first pack from line 34 and the new avatar img element
-    const fullComment = document.createElement("div");
-    fullComment.classList.add("firstComment");
-    // create new avatar img
-    const newAvatar = document.createElement("img");
-    newAvatar.src="/Graphs/main_avatar.jpg";
-    newAvatar.classList.add('mainAvatar');
-    // line 56 appent above avatar img, line 57 append the pack created in line 34
-    fullComment.appendChild(newAvatar);
-    fullComment.appendChild(userAndComment);
-    // add the full content in html post
-    commentsSection.appendChild(fullComment);
+        // crearea celui deal doilea div care cuprinde divul din linia 45 + poza de profil
+        const fullComment = document.createElement("div");
+        fullComment.classList.add("firstComment");
+        // creare avatar nou
+        const newAvatar = document.createElement("img");
+        newAvatar.src="/Graphs/main_avatar.jpg";
+        newAvatar.classList.add('mainAvatar');
+        // adaugare in divul "main" primul div si poza de avatar creata 
+        fullComment.appendChild(newAvatar);
+        fullComment.appendChild(userAndComment);
+        // adaugarea divului "main" in html
+        commentsSection.appendChild(fullComment);
 
-    // Increment the number of comments when a new comment is entered
-    totalComments++;
-    numberOfComments.textContent = totalComments;
+        // cresterea numarului de comentarii de fiecare data cand un comentariu este inserat
+        totalComments++;
+        numberOfComments.textContent = totalComments;
+
+        // reseteaza inputul din comment
+        postCommentInput.value = "";
+    } else {
+        return;
+    }
 }
+
+//  Schiumbare story ===================================
+
+let actualMovmentProgress = 0;
+
+function nextStory() {
+    // identificare latime story
+    const storyWidth = storyPost.offsetWidth;
+    // identificare latime vizibila
+    const visibleWidth = fullStories.offsetWidth;
+
+    actualMovmentProgress = actualMovmentProgress + storyWidth;
+
+    // 1268px total stories, minus 170px = 1098 px folosibili ca view, daca este sub 1000px, storiurile se duc in afara range ului
+
+    console.log(storyWidth);
+
+    storiesContainer.style.transform = `translateX(-${actualMovmentProgress}px)`;
+
+    prevStoryBtn.style.display = "block";
+    prevStoryBtn.style.zIndex = "100";
+
+}
+
 
 likeButton.addEventListener('click', likesManager);
 closePost.addEventListener("click", postClose);
+nextStoryBtn.addEventListener("click", nextStory);
+nextStoryBtnTwo.addEventListener("click", nextStory);
+
 commentSubmit.addEventListener("click", addComment);
+postCommentInput.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        addComment();
+    }
+});
--- open_app/vendor.js
+++ open_app/vendor.js
@@ -1,14 +1,25 @@
 // User
 const mainUserName = document.getElementById("mainUser");
+
 // Likes
 const totalLikes = document.getElementById("totalLikes");
 const totalLikesText = document.getElementById("likeBtnText");
 const likeButton = document.getElementById("actionLike");
-// Flow posts
+
+// Postari
 const closePost = document.getElementById("closePostBtn");
 const mainPost = document.getElementById("mainPost");
-// Comments from posts
+
+// Commentarii postari
 const postCommentInput = document.getElementById("commentInput");
 const commentSubmit = document.getElementById("commentSubmit");
 const commentsSection = document.getElementById("commentsSection");
 const numberOfComments = document.getElementById("numberOfComments");
+
+// Story HTML line 135 - 222
+const nextStoryBtn = document.getElementById("nextStoryIn");
+const nextStoryBtnTwo = document.getElementById("nextStoryInTwo");
+const prevStoryBtn = document.getElementById("prevStory");
+const storiesContainer = document.getElementById("stories");
+const storyPost = document.getElementById("publicPostOne");
+const fullStories = document.getElementById("popularUploads");
